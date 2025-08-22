da7adeb3f8099d82f8ba60516edc7a3cb450aaff
Roiko <roitaba1996@gmail.com>
2025-08-22 14:25:10 +0300

    Update Volume.py

--- Volume.py/Volume.py
+++ Volume.py/Volume.py
@@ -7,21 +7,22 @@ devices = AudioUtilities.GetSpeakers()
 interface = devices.Activate(IAudioEndpointVolume._iid_, comtypes.CLSCTX_ALL, None)
 volume = cast(interface, POINTER(IAudioEndpointVolume))
 
-def increase_volume():
+def increase_volume():                              # פונקציית העלאה..
     current_volume = volume.GetMasterVolumeLevel()
     new_volume = min(current_volume + 2.0, 0.0)
     volume.SetMasterVolumeLevel(new_volume, None)
     print("Volume increased. Current level:", new_volume)
 
-def decrease_volume():
+def decrease_volume():                   # פונקציית הורדה..
     current_volume = volume.GetMasterVolumeLevel()
     new_volume = max(current_volume - 2.0, -65.25)
     volume.SetMasterVolumeLevel(new_volume, None)
     print("Volume decreased. Current level:", new_volume)
 
-keyboard.add_hotkey('-', decrease_volume)
+keyboard.add_hotkey('-', decrease_volume)        # המקשים עצמם בהם רציתי להשתמש :)
 keyboard.add_hotkey('+', increase_volume)
 
-print("script is running..Press ESC to exit. tnx for me | you welcome (roiko:))")
+print("script is running..Press ESC to exit. tnx for me | you welcome (roiko:))") # הודעת פרידה 
 keyboard.wait('esc')
+
 
